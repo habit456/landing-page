76b2f971ca5e51b73bde31f4171432ae1f16ae89
habit456 <48520483+habit456@users.noreply.github.com>
2020-08-26 23:10:59 -0400

    added functionality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// TODO: add button functionality
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
+
+// Toggle nav
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
+
+// Open modal
+open.addEventListener("click", () => {
+  modal.classList.add("show-modal");
+});
+
+// Hide modal by clicking close
+close.addEventListener("click", () => {
+  modal.classList.remove("show-modal");
+});
+
+// Hide modal by clicking outside of it
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.classList.remove("show-modal");
+  }
+});
